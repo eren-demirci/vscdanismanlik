7f81e4b385a76adf764c091aaa80b3c3baee684c
Eren Demirci <lapdijital@gmail.com>
2026-05-04 12:29:47 +0300

    first commit

---
+++ data/sections/heroSlider2Data.ts
@@ -0,0 +1,45 @@
+export const HeroSlides2Data = [
+    {
+      subheading: "welcome to Zentra",
+      heading: "Business Strategy That Works",
+      text: "Our consulting services focus on smart planning, data-driven decisions, and practical solutions that help your business grow with confidence.",
+      button: {
+          label: "Get Started",
+          href: "/contact-us",
+          type: "secondary"
+      },
+      image: "/img/slider/hero-4.jpg",
+      imageTablet: "/img/slider/hero-4-991.jpg",
+      imageMobile: "/img/slider/hero-4-575.jpg",
+      thumb: "/img/slider/s4.jpg",
+    },
+    {
+      subheading: "welcome to Zentra",
+      heading: "Financial Clarity with Data",
+      text: "We analyze financial data and key metrics to transform complex numbers into clear strategies that support smarter decisions and long-term business success",
+      button: {
+          label: "Get Started",
+          href: "/contact-us",
+          type: "secondary"
+      },
+      image: "/img/slider/hero4-2.jpg",
+      imageTablet: "/img/slider/hero4-2-991.jpg",
+      imageMobile: "/img/slider/hero4-2-575.jpg",  
+      thumb: "/img/slider/s4-2.jpg"
+    },
+    {
+      subheading: "welcome to Zentra",
+      heading: "Planning for Smarter Growth",
+      text: "We help businesses create clear, practical plans that align strategy, resources, and goals to achieve sustainable and measurable growth.",
+      button: {
+          label: "Get Started",
+          href: "/contact-us",
+          type: "secondary"
+      },
+      image: "/img/slider/hero4-3.jpg",
+      imageTablet: "/img/slider/hero4-3-991.jpg",
+      imageMobile: "/img/slider/hero4-3-575.jpg",    
+      thumb: "/img/slider/s4-3.jpg"
+    }
+
+];
